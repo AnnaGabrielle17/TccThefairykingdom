a1aa83ad98bfcc1d4bcf9c63f1ce7bac867a8e0d
AnnaGabrielle17 <145567496+AnnaGabrielle17@users.noreply.github.com>
2025-09-18 15:35:55 -0300

    fase1quasepronta

---
+++ tccthefairykingdom/Assets/Scripts/CenarioInfinito.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CenarioInfinito : MonoBehaviour
+{
+    public float velocidadeDoCenario;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        MovimentarCenario();
+    }
+    private void MovimentarCenario()
+    {
+        Vector2 deslocamentoDoCenario = new Vector2(Time.time * velocidadeDoCenario, 0f);
+        GetComponent<Renderer>().material.mainTextureOffset = deslocamentoDoCenario;
+    }
+}
--- tccthefairykingdom/Assets/Scripts/FairyController.cs
+++ tccthefairykingdom/Assets/Scripts/FairyController.cs
@@ -2,11 +2,18 @@ using UnityEngine;
 
 public class FairyController : MonoBehaviour
 {
-     private Animator anim;
+    private Animator anim;
 
     void Start()
     {
         anim = GetComponent<Animator>();
         anim.Play("Fly"); // já começa voando
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger("Attack");
+        }
     }
 }
